73adaf0a21a11940ccc613c91ce4aba90f7f2aaf
omar abu assaf <omarkamalabuassaf1@gmail.com>
2025-01-30 15:05:52 +0300

    trying normal chromedriver

--- scrape_n_store.py
+++ scrape_n_store.py
@@ -10,24 +10,24 @@ import time
 from save_to_bucket import save_to_storage
 import logging
 from selenium.webdriver.chrome.options import Options
-# from undetected_chromedriver import Chrome, ChromeOptions
+# from undetected_chromedriver import Chrome, ChromeOptions test2 
 
 
 logging.basicConfig(level=logging.INFO)
 
-# chrome_options = webdriver.ChromeOptions()
-chrome_options = Options()
+chrome_options = webdriver.ChromeOptions()
+#chrome_options = Options()
 chrome_options.add_argument("--headless")
 #chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--log-level=1")
-driver = webdriver.Chrome(options=chrome_options)
+#driver = webdriver.Chrome(options=chrome_options)
 # driver.get("https://tenders.etimad.sa/Tender/AllTendersForVisitor?PageNumber=1")
 
-driver.set_page_load_timeout(300)  # Increase the timeout for page load
-driver.set_script_timeout(300)
+# driver.set_page_load_timeout(300)  # Increase the timeout for page load
+# driver.set_script_timeout(300)
 
 def post_process_results(term_tenders):
     logging.info("got etimad website successfully!!!")
@@ -137,7 +137,7 @@ def start_parsing(term_tenders, driver):
     return
 def setup_search(main_activityy):
     logging.info("Starting the scraper...")
-    # driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
     logging.info("Driver initialized, navigating to website...")
     # driver.get("https://tenders.etimad.sa/Tender/AllTendersForVisitor?PageNumber=1")
