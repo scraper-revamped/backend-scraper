5bd7437646e39fa3f85ef41d76380fa6bff52c2d
omar abu assaf <omarkamalabuassaf1@gmail.com>
2025-02-05 11:08:17 +0300

    test, please work

--- app.py
+++ app.py
@@ -89,5 +89,6 @@ def scrape():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 8080))
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
+    # app.run(debug=True)
