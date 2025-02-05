6f8378689c1e3a08474e146f7baf2f23b22f0692
omar abu assaf <omarkamalabuassaf1@gmail.com>
2025-02-05 12:47:32 +0300

    test on  dif site

--- scrape_n_store.py
+++ scrape_n_store.py
@@ -177,7 +177,8 @@ def setup_search(main_activityy):
         # driver = setup_chrome_driver()
         logging.info("Driver initialized, navigating to website...")
         
-        website_url = "https://tenders.etimad.sa/Tender/AllTendersForVisitor?PageNumber=1"
+        website_url = "https://roya.tv/"
+        # https://tenders.etimad.sa/Tender/AllTendersForVisitor?PageNumber=1
         driver.get(website_url)
         logging.info("got etimad website successfully!!!")
         
