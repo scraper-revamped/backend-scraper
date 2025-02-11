a4f264c1d861a70e01bd41ac8872b272c1bf67fc
omar abu assaf <omarkamalabuassaf1@gmail.com>
2025-02-11 12:14:32 +0300

    changed bucket for new acc

--- save_to_bucket.py
+++ save_to_bucket.py
@@ -37,7 +37,7 @@ def save_to_storage(df, term, username):
         df.to_excel(file_name, index=False)
         logging.info(f"File saved locally: {file_name}")
 
-        bucket_name = "tenders-excel-files"
+        bucket_name = "scraping_revamped"
         logging.info(f"deleting file ....: {term}/{file_name}")
         delete_existing_files(bucket_name)
         logging.info(f"file deleted ....: {term}/{file_name}")
