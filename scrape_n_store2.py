a4f809f104425f4f54e642277e86b4bdbacf2cf5
omar abu assaf <omarkamalabuassaf1@gmail.com>
2025-04-29 15:14:40 +0300

    increased timeout to load site

--- scrape_n_store2.py
+++ scrape_n_store2.py
@@ -191,7 +191,7 @@ def setup_search(main_activityy):
         website_url = "https://tenders.etimad.sa/Tender/AllTendersForVisitor?PageNumber=1"
         driver.get(website_url)
         logging.info("got etimad website successfully!!!")
-        time.sleep(180)
+        time.sleep(300)
         # expand search
         search_button = driver.find_element(By.XPATH, "//*[@id='searchBtnColaps']")
         search_button.click()
