ef4dd22a5dffc3952d8fd3ec72a78b6b441014af
omar abu assaf <omarkamalabuassaf1@gmail.com>
2025-05-12 11:49:48 +0300

    modified bucket name for new GCP acc

--- save_to_bucket.py
+++ save_to_bucket.py
@@ -41,7 +41,7 @@ def save_to_storage(df, term, username):
         df.to_excel(file_name, index=False)
         logging.info(f"File saved locally: {file_name}")
 
-        bucket_name = "scraping_revamped"
+        bucket_name = "scraping_revamped_2"
         logging.info(f"deleting file ....: {term}/{file_name}")
         delete_existing_files(bucket_name)
         logging.info(f"file deleted ....: {term}/{file_name}")
