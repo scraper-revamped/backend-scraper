a6f15ea9fa922c2b37c1d9550cb899e4cb2581e9
omar abu assaf <omarkamalabuassaf1@gmail.com>
2025-09-01 01:01:44 +0300

    testing the profile error

--- scrape_n_store_tst.py
+++ scrape_n_store_tst.py
@@ -12,6 +12,8 @@ import logging
 # from bs4 import BeautifulSoup
 import requests
 import bs4
+import tempfile
+from pathlib import Path
 
 
 logging.basicConfig(level=logging.INFO)
@@ -23,6 +25,9 @@ chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--log-level=1")
+tmp_profile = str(Path(tempfile.mkdtemp()).resolve())
+arg = f'--user-data-dir="{tmp_profile}"'
+chrome_options.add_argument(arg)
 # chrome_options.add_argument("--disable-blink-features=AutomationControlled")  
 chrome_options.page_load_strategy = 'none'
 
