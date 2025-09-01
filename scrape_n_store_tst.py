324fc3041d355226848ac95b0e3b2682bd702088
omar abu assaf <omarkamalabuassaf1@gmail.com>
2025-09-01 04:07:31 +0300

    trying to solve the locked profile directory

--- scrape_n_store_tst.py
+++ scrape_n_store_tst.py
@@ -201,6 +201,8 @@ def start_parsing(term_tenders, driver, max_retries=3):
 
 def setup_search(main_activityy):
     logging.info("Starting the scraper...")
+    user_data_dir = tempfile.mkdtemp()  # unique temp directory per session
+    chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
     driver = webdriver.Chrome(options=chrome_options)
     # driver.set_page_load_timeout(300)  # Set timeout for page loading
     # driver.set_script_timeout(300) 
@@ -271,6 +273,7 @@ def setup_search(main_activityy):
     finally:
         # Guaranteed cleanup
         driver.quit()
+        shutil.rmtree(user_data_dir, ignore_errors=True)
         # if user_data_dir and os.path.exists(user_data_dir):
         #     shutil.rmtree(user_data_dir)
 
