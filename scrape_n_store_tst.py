a2f246774167d596bd12dee8c179ff8f909be03c
omar abu assaf <omarkamalabuassaf1@gmail.com>
2025-09-01 03:09:51 +0300

    removing the profile code

--- scrape_n_store_tst.py
+++ scrape_n_store_tst.py
@@ -18,8 +18,8 @@ import shutil
 
 
 logging.basicConfig(level=logging.INFO)
-user_data_dir = None
-user_data_dir = tempfile.mkdtemp()
+# user_data_dir = None
+# user_data_dir = tempfile.mkdtemp()
 # chrome_options = ChromeOptions()
 chrome_options = webdriver.ChromeOptions()
 #chrome_options.add_argument("--headless")
@@ -27,7 +27,7 @@ chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--log-level=1")
-chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+# chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 # chrome_options.add_argument("--disable-blink-features=AutomationControlled")  
 chrome_options.page_load_strategy = 'none'
 
@@ -272,8 +272,8 @@ def setup_search(main_activityy):
         # Guaranteed cleanup
         if driver:
             driver.quit()
-        if user_data_dir and os.path.exists(user_data_dir):
-            shutil.rmtree(user_data_dir)
+        # if user_data_dir and os.path.exists(user_data_dir):
+        #     shutil.rmtree(user_data_dir)
 
 # if __name__ == "__main__":
 #     setup_search("الاتصالات وتقنية المعلومات")
